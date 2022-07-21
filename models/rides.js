b8227553cb91a023a690bf7478271c7a0704d534
Austin Do <austinndo@gmail.com>
2022-07-21 12:52:37 -0700

    Data seeded, first routers and controllers are set up

--- models/rides.js
+++ models/rides.js
@@ -1,11 +1,11 @@
 const { Schema } = require('mongoose')
 
 const Ride = new Schema(
-    {
-        name: { type: String, required: true},
-        category: {type: String, required: true},
-        times_slot: { type: String, required: true}
-    },
-    { timestamps: true }
+  {
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    times_slot: { type: Array, required: true }
+  },
+  { timestamps: true }
 )
 module.exports = Ride
